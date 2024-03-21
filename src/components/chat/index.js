8b2444ee13a44a581eb9a6db3e66d37769253c7d
Sai Bingi <saibingi1208@gmail.com>
2024-03-21 12:04:36 +0530

    arrow-left

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,39 +1,57 @@
 import { Input } from "../commonComponents/commonComponents";
-import { useState, useEffect } from "react";
-import { MdOutlineSearch } from "react-icons/md";
-import { GoPlus } from "react-icons/go";
-import { IoIosNotificationsOutline } from "react-icons/io";
-import { BsMicMute } from "react-icons/bs";
-import { IoVideocamOutline } from "react-icons/io5";
-import { IoCallOutline } from "react-icons/io5";
-import { PiDotsThreeVerticalBold } from "react-icons/pi";
-import { IoMdCall } from "react-icons/io";
+import { useState } from "react";
+import Tooltip from "@mui/material/Tooltip";
 import Image from "next/image";
 import url1 from "../../assets/images/profile1.svg";
 import url2 from "../../assets/images/profile2.svg";
 import url3 from "../../assets/images/profile3.svg";
-import MsgSeen from "../svgComponents/index.js";
-import { ArrowLeft, ArrowDown } from "../svgComponents/index.js";
+import url4 from "../../assets/images/recentUser1.svg";
+import url5 from "../../assets/images/recentUser2.svg";
+import url6 from "../../assets/images/recentUser3.svg";
+import url7 from "../../assets/images/chatProfile.svg";
+import {
+  Badge1,
+  Badge2,
+  Badge3,
+  Badge4,
+  Search,
+  Plus,
+  ChatAudio,
+  ChatVideo,
+  ThreeDots,
+  Notification,
+  Mute,
+  Video,
+  EndCall,
+  MsgSeen,
+  Attachment,
+  ArrowLeft,
+  ArrowDown,
+  Send,
+} from "../svgComponents/index.js";
 import "./index.css";
 
 const recentUsers = [
   {
     id: 1,
+    profileIcon: url4,
     onlineStatus: true,
   },
   {
     id: 2,
+    profileIcon: url5,
     onlineStatus: false,
   },
   {
     id: 3,
+    profileIcon: url6,
     onlineStatus: true,
   },
 ];
 
 const notification = true;
 
-const chatList = [
+let chatList = [
   {
     id: 1,
     message:
@@ -73,7 +91,7 @@ const chatList = [
   {
     id: 5,
     message:
-      "Hi there , nice to meet you, My name is Jenny Wilson, and I’m from Jakrta",
+      "https://res.cloudinary.com/dysnxt2oz/image/upload/v1710821971/Rectangle_39_vy2nfe.png",
     date: new Date(),
     sender: 1,
     receiver: 2,
@@ -82,7 +100,7 @@ const chatList = [
   {
     id: 6,
     message:
-      "https://res.cloudinary.com/dysnxt2oz/image/upload/v1710821971/Rectangle_39_vy2nfe.png",
+      "Hi there , nice to meet you, My name is Jenny Wilson, and I’m from Jakrta",
     date: new Date(),
     sender: 2,
     receiver: 1,
@@ -166,15 +184,21 @@ const chats = [
   },
 ];
 
+const friends = chats;
+
 const updateChatList = (chatList) => {
-  const today = new Date("2024-03-15");
+  const today = new Date();
   const yesterday = new Date(today);
   yesterday.setDate(today.getDate() - 1);
+  const dayBeforeYesterday = new Date(yesterday);
+  dayBeforeYesterday.setDate(yesterday.getDate() - 1);
 
   let currentDate;
 
   chatList.forEach((message, index) => {
-    if (index < 3) {
+    if (index < 2) {
+      currentDate = dayBeforeYesterday;
+    } else if (index < 4) {
       currentDate = yesterday;
     } else {
       currentDate = today;
@@ -196,30 +220,50 @@ const updateChatList = (chatList) => {
     message.date = formattedDate;
   });
 
-  chatList.sort((a, b) => a.date - b.date);
-
   return chatList;
 };
 
-const updatedChatList = updateChatList(chatList);
+chatList.sort((a, b) => a.date - b.date);
+
+chatList = updateChatList(chatList);
+
+let lastDisplayedDate = null;
 
 const Chat = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [message, setMessage] = useState("");
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleArrowSize = () => {
+    setIsExpanded((prevState) => !prevState);
+  };
 
   const getLastChat = (chatList, index) => {
     if (index < chatList.length - 1) {
-      if (chatList[index].sender === 1) {
-        return chatList[index].sender === 1 && chatList[index + 1].sender === 1;
-      } else {
-        return chatList[index].sender === 2 && chatList[index + 1].sender === 2;
-      }
+      return chatList[index].sender === chatList[index + 1].sender;
     }
   };
 
-  const getLastDate = (chatList, index) => {
+  // const getLastDate = (chatList, index) => {
+  //   if (index < chatList.length - 1) {
+  //     return chatList[index].date === chatList[index + 1].date;
+  //   }
+  // };
+
+  const getLastDate = (chatList, index, prevDate) => {
     if (index < chatList.length - 1) {
-      return chatList[index].date === chatList[index + 1].date;
+      const currentDate = chatList[index].date;
+      const nextDate = chatList[index + 1].date;
+      const isNewDate = currentDate !== nextDate;
+      const isDifferentFromPrev = currentDate !== prevDate;
+
+      // Update the previous date
+      prevDate = currentDate;
+
+      return isNewDate && isDifferentFromPrev;
     }
+
+    return true; // Always show the date for the last chat
   };
 
   return (
@@ -231,11 +275,14 @@ const Chat = () => {
           <div className="recent-user-con">
             {recentUsers.map((eachUser) => (
               <div key={eachUser.id} className="recent-user">
-                <div className="user-icon">
-                  {eachUser.onlineStatus ? (
-                    <span className="green-dot"></span>
-                  ) : null}
-                </div>
+                <Image
+                  src={eachUser.profileIcon}
+                  alt="recent-user-icon"
+                  className="user-icon"
+                />
+                {eachUser.onlineStatus ? (
+                  <span className="green-dot"></span>
+                ) : null}
               </div>
             ))}
           </div>
@@ -247,17 +294,14 @@ const Chat = () => {
               placeholder="Search"
               value={searchInput}
             />
-            <MdOutlineSearch size={18} className="search-icon" />
+            <Search />
           </div>
           <div className="new-chats-con">
             <p className="new-chat">New Chats</p>
-            <GoPlus className="plus-icon" />
+            <Plus />
           </div>
           <div className="notification-icon-con">
-            <IoIosNotificationsOutline
-              className="notification-icon"
-              size={22}
-            />
+            <Notification />
             {notification ? <span className="red-dot"></span> : null}
           </div>
         </div>
@@ -277,33 +321,103 @@ const Chat = () => {
             </div>
             <div className="call-controllers">
               <div className="calls">
-                <BsMicMute className="mute-icon" size={18} />
-                <IoVideocamOutline className="video-icon" size={18} />
-                <IoCallOutline className="end-call" size={14} />
+                <Mute />
+                <Video />
+                <EndCall />
               </div>
             </div>
           </div>
-          <div className="chat-container">
+          <div className={`chat-container ${isExpanded ? "expand" : ""}`}>
             <div className="chat-header">
               <div className="icon-username">
-                <div className="chat-icon"></div>
+                <Image
+                  src={url7}
+                  alt="chat-profile-icon"
+                  className="chat-icon"
+                />
                 <div className="user-time">
                   <p className="chat-username">Jenny Wilson</p>
+                  <div className="badges-container">
+                    <Tooltip
+                      title="Badge1"
+                      placement="bottom"
+                      enterDelay={400}
+                      leaveDelay={200}
+                    >
+                      <div>
+                        <Badge1 />
+                      </div>
+                    </Tooltip>
+                    <Tooltip
+                      title="Badge2"
+                      placement="bottom"
+                      enterDelay={400}
+                      leaveDelay={200}
+                    >
+                      <div>
+                        <Badge2 />
+                      </div>
+                    </Tooltip>
+                    <Tooltip
+                      title="Badge3"
+                      placement="bottom"
+                      enterDelay={400}
+                      leaveDelay={200}
+                    >
+                      <div>
+                        <Badge3 />
+                      </div>
+                    </Tooltip>
+                    <Tooltip
+                      title="Vibe Master"
+                      placement="bottom"
+                      enterDelay={400}
+                      leaveDelay={200}
+                    >
+                      <div>
+                        <Badge4 />
+                      </div>
+                    </Tooltip>
+                    <p className="five-plus">5+</p>
+                  </div>
                   <p className="minutes">Minutes Ago</p>
                 </div>
               </div>
+              <div className="accept-reject">
+                <button type="button" className="accept">
+                  Accept
+                </button>
+                <button type="button" className="reject">
+                  Regret
+                </button>
+              </div>
               <div className="chat-controllers">
-                <IoMdCall className="call-icon" size={20} />
-                <IoVideocamOutline className="video-icon" size={20} />
-                <PiDotsThreeVerticalBold className="three-dots" size={20} />
+                <ChatAudio />
+                <ChatVideo />
+                <ThreeDots />
               </div>
             </div>
-            <ArrowLeft className="arrow-left" />
+            <div className="arrow-container" onClick={toggleArrowSize}>
+              <ArrowLeft className="arrow-left" />
+            </div>
             <div className="chat-container-2">
               {chatList.map((chat, index) => {
+                const prevDate = index > 0 ? chatList[index - 1].date : null;
                 const isLastChat = getLastChat(chatList, index);
-                const isLastDate = getLastDate(chatList, index);
+                const isLastDate = getLastDate(chatList, index, prevDate);
                 const isImage = chat.message.startsWith("http");
+
+                // Check if the current date is today
+                const today = new Date();
+                const isToday =
+                  today.toDateString() === new Date(chat.date).toDateString();
+
+                // Check if the current date is yesterday
+                const yesterday = new Date();
+                yesterday.setDate(yesterday.getDate() - 1);
+                const isYesterday =
+                  yesterday.toDateString() ===
+                  new Date(chat.date).toDateString();
 
                 let chatDate = chat.date.split(";")[0];
                 chatDate = chatDate.split(",");
@@ -311,31 +425,55 @@ const Chat = () => {
                   chatDate[1].split(" ")[2]
                 } ${chatDate[1].split(" ")[1]} ${chatDate[2]}`;
 
+                // Check if the current date is different from the last displayed date
+                const isNewDate = lastDisplayedDate !== formattedDate;
+
+                if (isNewDate) {
+                  lastDisplayedDate = formattedDate;
+                }
+
                 return (
                   <div key={chat.id}>
-                    {isLastDate && isLastChat && (
-                      <p className="chat-date">{formattedDate}</p>
+                    {isNewDate && (
+                      <p className="chat-date">
+                        {isToday
+                          ? "Today"
+                          : isYesterday
+                          ? "Yesterday"
+                          : formattedDate}
+                      </p>
                     )}
                     <div
                       className={`msg-con ${
-                        isLastDate && isLastChat ? "margin2" : "margin1"
+                        isLastChat ? "margin2" : "margin1"
                       }`}
                     >
                       <div
-                        className={`msg-profile ${
-                          chat.sender === 2
-                            ? "sender sent"
-                            : "receiver received"
+                        className={`${
+                          chat.sender === 2 ? "sender" : "receiver"
                         }`}
                       >
                         {isImage ? (
                           <img
-                            className="chat-img"
+                            className={`chat-img ${
+                              chat.sender === 2 ? "message-1" : "message-2"
+                            }`}
                             src={chat.message}
                             alt="chat-img"
                           />
                         ) : (
-                          <p className="message">{chat.message}</p>
+                          <div
+                            className={`msg-con-seen ${
+                              chat.sender === 2 ? "message-1" : "message-2"
+                            }`}
+                          >
+                            <p className="message">{chat.message}</p>
+                            {chat.sender === 2 && (
+                              <div className="msg-seen-icon-con">
+                                <MsgSeen />
+                              </div>
+                            )}
+                          </div>
                         )}
                       </div>
                       {!isLastChat && (
@@ -374,8 +512,23 @@ const Chat = () => {
                 );
               })}
             </div>
+            <div className="send-msg-con-1">
+              <div className="send-msg-con-2">
+                <div className="attach-type">
+                  <Attachment />
+                  <Input
+                    type="text"
+                    className="send-msg"
+                    placeholder="Type a message"
+                    value={message}
+                    onChange={setMessage}
+                  />
+                </div>
+                <Send />
+              </div>
+            </div>
           </div>
-          <div className="friends-container">
+          <div className={`friends-container ${isExpanded ? "expanded" : ""}`}>
             <div>
               <div className="chats-arrow">
                 <p className="chat-text">Chats</p>
@@ -389,7 +542,27 @@ const Chat = () => {
                   }`}
                 >
                   <div className="profile">
-                    <Image src={url3} alt="friend-profile" />
+                    <Image src={url3} width={50} alt="friend-profile" />
+                    <div className="profile-name-desc">
+                      <p className="profile-name">{eachUser.name}</p>
+                      <p className="profile-description">
+                        {eachUser.description}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="profile-time">{eachUser.lastTime}</p>
+                </ul>
+              ))}
+            </div>
+            <div>
+              <div className="chats-arrow">
+                <p className="chat-text">Friends</p>
+                <ArrowDown />
+              </div>
+              {friends.map((eachUser, index) => (
+                <ul key={index} className="profile-info">
+                  <div className="profile">
+                    <Image src={url3} width={50} alt="friend-profile" />
                     <div className="profile-name-desc">
                       <p className="profile-name">{eachUser.name}</p>
                       <p className="profile-description">
