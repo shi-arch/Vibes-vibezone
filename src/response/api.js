9bc3adaa0816c40dc25a917e66a8b69501674b42
Shivram kashyap <kashyapshivram512@gmail.com>
2024-03-27 11:50:31 +0530

    video call

--- src/response/api.js
+++ src/response/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const getApi = async (url) => {
-    const response = await axios.get(process.env.NEXT_PUBLIC_LOCAL_BASEURL +'/api'+url)
+    const response = await axios.get(process.env.NEXT_PUBLIC_SERVER_BASEURL +'/api'+url)
     return response.data
 }
 
 export const postApi = async (url, data) => {
-    const response = await axios.post(process.env.NEXT_PUBLIC_LOCAL_BASEURL +'/api'+url, data)
+    const response = await axios.post(process.env.NEXT_PUBLIC_SERVER_BASEURL +'/api'+url, data)
     return response.data
 }
