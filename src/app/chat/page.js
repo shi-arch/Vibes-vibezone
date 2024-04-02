b095f2e39af9f727ca3b39f751ef47fc098142e1
Shivram kashyap <kashyapshivram512@gmail.com>
2024-04-02 19:40:12 +0530

    data re rendering

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,16 +1,26 @@
 
 import Chat from '../../components/chat'
-import SideBar from "../../components/SideBar/sideBar";
 import PreferenceModal from "../../components/Modals/PreferenceModal";
 import ProfileModal from "../../components/Modals/ProfileModal";
 import BadgesModal from "../../components/Modals/BadgesModal";
-import PricingPlansModal from "../../components/Modals/PricingPlansModal";
-import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import PrivacyPolicyModal from "../../components/Modals/PrivacyPolicyModal";
 import NotificationModal from "../../components/Modals/NotificationModal";
 import BillingModal from "../../components/Modals/BillingModal";
+import React, { useEffect } from 'react'
+import { setLoginDetails, setToken } from "../../redux/features/loginSlice";
 
 const Page = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  let userData = localStorage.getItem("userData")
+  if(userData){
+    const parsedData = JSON.parse(userData)
+    dispatch(setLoginDetails(parsedData.user));
+    dispatch(setToken(parsedData.token));
+  }  
+  }, []);
     return (
       <div>
         <Chat />
