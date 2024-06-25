48a47861fb8ad661e99824838b49cc03ed185226
Shivram kashyap <kashyapshivram512@gmail.com>
2024-06-25 16:19:45 +0530

    commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,12 +14,14 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
+if(messaging.isSupported()){
+  messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message: ', payload);
+  
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = { body: payload.notification.body };
+  
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
+}
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message: ', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = { body: payload.notification.body };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
